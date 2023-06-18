162a6619c46f58ed72a271a46f0e0506fa42c9fd
AdamKaluza <56393414+AdamKaluza@users.noreply.github.com>
2023-06-18 20:04:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# biu_zad8.2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-7dksm3)
---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, useLocation, Routes } from 'react-router-dom';
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
+const Add = () => {
+  const query = useQuery();
+  const x = Number(query.get('x'));
+  const y = Number(query.get('y'));
+  return <h2>{x} + {y} = {x + y}</h2>
+}
+
+const Subtract = () => {
+  const query = useQuery();
+  const x = Number(query.get('x'));
+  const y = Number(query.get('y'));
+  return <h2>{x} - {y} = {x - y}</h2>
+}
+
+const Multiply = () => {
+  const query = useQuery();
+  const x = Number(query.get('x'));
+  const y = Number(query.get('y'));
+  return <h2>{x} * {y} = {x * y}</h2>
+}
+
+const Divide = () => {
+  const query = useQuery();
+  const x = Number(query.get('x'));
+  const y = Number(query.get('y'));
+  return <h2>{x} / {y} = {x / y}</h2>
+}
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/add" element={<Add />} />
+        <Route path="/sub" element={<Subtract />} />
+        <Route path="/mul" element={<Multiply />} />
+        <Route path="/div" element={<Divide />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
